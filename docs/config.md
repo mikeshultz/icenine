33f87dc6db2ca824caf5bc7e1c082272543eb2fc
Mike Shultz <shultzm@gmail.com>
2017-08-09 16:51:56 -0600

    Added loglevel as configuration option and documented config

---
+++ docs/config.md
@@ -0,0 +1,12 @@
+# Icenine Configuration
+
+Configuration is located in `%APPDATA%/icenine/icenine.ini` on Windows and 
+`~/.config/icenine/icenine.ini` on everything else.
+
+## INI Options
+
+The following are all of the options for the config.  All are optional.
+
+    [default]
+    logfile = /var/log/icenine.log
+    loglevel = warning
--- icenine/core/__init__.py
+++ icenine/core/__init__.py
@@ -3,12 +3,24 @@ import logging as log
 import configparser
 
 CONFIG = configparser.ConfigParser()
-CONFIG.read(os.path.expanduser('~/.config/icenine/icenine.ini'))
+if sys.platform == 'win32':
+    confPath = '~/AppData/icenine/icenine.ini'
+else:
+    confPath = '~/.config/icenine/icenine.ini'
+CONFIG.read(os.path.expanduser(confPath))
 
-
-bcKwargs = {
-    "level": log.DEBUG
+LOG_LEVEL_TRANSLATE = {
+    'debug': log.DEBUG,
+    'info': log.INFO,
+    'warning': log.WARNING,
+    'error': log.ERROR,
+    'critical': log.CRITICAL,
 }
+
+bcKwargs = {}
+
+# Set loglevel
+bcKwargs['level'] = LOG_LEVEL_TRANSLATE[CONFIG.get('default', 'loglevel', fallback='warning')]
 
 # Try and set logfile location if available
 try:
