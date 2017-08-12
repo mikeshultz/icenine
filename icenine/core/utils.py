7a381b7dca7ef10de3079a7fa13d56b77d5318b4
Mike Shultz <shultzm@gmail.com>
2017-08-12 02:17:56 -0600

    Added to_string and privtoaddr for getting addresses from private keys

--- icenine/core/utils.py
+++ icenine/core/utils.py
@@ -1,10 +1,18 @@
 import re
 import datetime
+import bitcoin
 from uuid import uuid4
 from secp256k1 import PrivateKey, PublicKey
+from eth_utils import to_normalized_address
+from Crypto.Hash import keccak
+
+sha3_256 = lambda x: keccak.new(digest_bits=256, data=x).digest()
 
 UUID_REGEX = r'^[A-Fa-f0-9]{8}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{12}$'
 ADDRESS_REGEX = r'(0x[A-Za-z0-9]{40})'
+
+def sha3(seed):
+    return sha3_256(to_string(seed))
 
 def generate_uuid():
     """ Convert private key to a UUID """
@@ -17,6 +25,14 @@ def is_uuid(val):
         return True
     else:
         return False
+
+def to_string(value):
+    if isinstance(value, bytes):
+        return value
+    if isinstance(value, str):
+        return bytes(value, 'utf-8')
+    if isinstance(value, int):
+        return bytes(str(value), 'utf-8')
 
 def new_keypair():
     """ Create a new Ethereum-compatible keypair """
@@ -32,4 +48,10 @@ def extract_address(val):
         return match.group(1)
 
 def unix_time():
+    """ Get the current unix timestamp """
     return int(datetime.datetime.utcnow().timestamp())
+
+def privtoaddr(k):
+    """ Turn a private key into an ethereum address """
+    pk = PrivateKey(k)
+    return to_normalized_address(sha3(pk.pubkey.serialize(compressed=False)[1:])[12:])
