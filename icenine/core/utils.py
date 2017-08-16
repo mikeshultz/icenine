f01a238d2685b22d556b26efc08932ac08deacbc
Mike Shultz <shultzm@gmail.com>
2017-08-16 16:53:07 -0600

    Some cleanup, and a couple minor bugfixes

--- icenine/core/utils.py
+++ icenine/core/utils.py
@@ -1,7 +1,7 @@
 import re
 import datetime
 import bitcoin
-from uuid import uuid4
+from uuid import UUID, uuid4
 from secp256k1 import PrivateKey, PublicKey
 from eth_utils import to_normalized_address
 from Crypto.Hash import keccak
@@ -9,18 +9,19 @@ from Crypto.Hash import keccak
 sha3_256 = lambda x: keccak.new(digest_bits=256, data=x).digest()
 
 UUID_REGEX = r'^[A-Fa-f0-9]{8}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{12}$'
-ADDRESS_REGEX = r'(0x[A-Za-z0-9]{40})'
+ADDRESS_REGEX = r'(0x[A-Fa-f0-9]{40})'
 
 def sha3(seed):
     return sha3_256(to_string(seed))
 
 def generate_uuid():
     """ Convert private key to a UUID """
-    # TODO: Implement this
     return uuid4()
 
 def is_uuid(val):
     """ Check if string is a UUID """
+    if val and type(val) == UUID:
+        return True
     if not val or type(val) not in [str, bytes]:
         return False
     if re.match(UUID_REGEX, val.strip()):
@@ -44,10 +45,11 @@ def new_keypair():
 def extract_address(val):
     """ Pull an address out of a string """
 
-    match = re.match(ADDRESS_REGEX, val)
+    match = re.search(ADDRESS_REGEX, val)
 
     if match:
         return match.group(1)
+    return None
 
 def unix_time(micro=False):
     """ Get the current unix timestamp 
